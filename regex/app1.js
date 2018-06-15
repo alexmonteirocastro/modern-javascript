16a76bfb467973fe7068e4b4d0f83432b2c82c54
Alex Castro <alex.monteiro@outlook.pt>
2018-06-15 15:38:20 +0200

    update

--- regex/app1.js
+++ regex/app1.js
@@ -1,5 +1,22 @@
+let re;
+
+//Literal characters 
+re = /hello/;
+
+
+
+const str = 'Hello World';
+
+//Log results 
+const result = re.exec(str);
+console.log(result);
+
 function reTest(re, str){
     if(re.test(str)){
-        console.log(`${str}`);
+        console.log(`${str} matches ${re.source}`);
+    } else {
+        console.log(`${str} DOES NOT match ${re.source}`);
     }
 }
+
+reTest(re, str);
